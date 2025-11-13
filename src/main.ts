afc4c0cc7f93a5fc65b7070cfe077f4af231f015
Azeezbank <bankoleazeezb98@gmail.com>
2025-11-13 11:47:35 +0100

    updated

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  const port = Number(process.env.PORT) || 3000;
+  const port = Number(process.env.PORT) || 10000;
 await app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
 }
 bootstrap();
